51dadbac6f2f51caca494cc4d32215c01e1bacaa
jhsm croonen <info@jcroonen.nl>
2020-04-16 07:54:53 +0200

    icons

--- background.js
+++ background.js
@@ -17,21 +17,16 @@ chrome.runtime.onInstalled.addListener(function() {
 const winspecs = 'width=1300,height=790,resizable=0,locationbar=0,top=100,left=100';
 
 function callUrl(tab) {
-    // const pos = url.indexOf('?');
-    // const search = url.substr(pos);
-    // console.log('tab', tab);
-    console.log('activeReq', activeReq);
-    const {title, src} = activeReq;
-    // title = title
-    //     .replace(/ /g, '+');
-    // const wurl = `viewer.html?title=${title}&src=${src}`;
-    // // console.log(wurl);
-    const wurl = src;
-    window.open(
-        wurl,
-        'npo',
-        winspecs
-    );
+    // console.log('activeReq', activeReq);
+    if (activeReq) {
+        const {title, src} = activeReq;
+        const wurl = src;
+        window.open(
+            wurl,
+            'npo',
+            winspecs
+        );
+    }
 }
 
 //https://start-player.npo.nl/embed/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzbWFydHRhZyI6eyJzaXRlSWQiOiI0In0sImhhc1N1YnNjcmlwdGlvbiI6IjEiLCJoYXNQcmVtaXVtU3Vic2NyaXB0aW9uIjoiMSIsImVsZW1lbnRJZCI6InBsYXllci1MSV9OTDFfNDE4ODEwMiIsIm1lZGlhSWQiOiJMSV9OTDFfNDE4ODEwMiIsInRvcHNwaW4iOnsicGFydHlJZCI6IjA6azUxbXZsOGU6eU9wVlJpYTl6cExoRjd-M3BqbXNFNk1GM2ROZ2NRcDIiLCJwcm9maWxlSWQiOiIyZDBhMmU2YS00MDJhLTQ4NzItOWU4Ny1lNjAyNGRmMGE3YmQifSwiaGFzU2V0dGluZ3MiOiIxIiwic3R5bGVWZXJzaW9uIjoiMiIsImhhc0FkQ29uc2VudCI6IjAiLCJzaGFyZSI6IjAiLCJhdXRvcGxheSI6IjEiLCJwYWdlVXJsIjoiaHR0cDpcL1wvd3d3Lm5wb3N0YXJ0Lm5sXC9saXZlXC9ucG8tMSIsInN0ZXJSZWZlcnJhbFVybCI6Imh0dHA6XC9cL3d3dy5ucG9zdGFydC5ubFwvbGl2ZVwvbnBvLTEiLCJzdGVyU2l0ZUlkIjoibnBvc3RhcnQiLCJpYXQiOjE1ODcwMTEwNjUsIm5iZiI6MTU4NzAxMTA2NSwiZXhwIjoxNTg3MDM5ODY1LCJjb25zdW1lcklkIjpudWxsLCJpc1BsYXlsaXN0IjpmYWxzZSwicmVmZXJyZXJVcmwiOm51bGwsInNraXBDYXRhbG9nIjowLCJub0FkcyI6MCwicmVjb21tZW5kYXRpb25zIjoxLCJpc3MiOiJleUpwZGlJNklpdDNTSHB2VVhsRWFqWnBRVVJ4YUc5eGRWSXlhSGM5UFNJc0luWmhiSFZsSWpvaWIxZ3laSFJxTWpaSGEzQTRjRnA0V0c0NGVrb3hWVzh5VERCcVQzcDFSMkYwZVhCeVVFVnZNVzQxTWs0NE5tUjNVMkY0Y0hab2F6WlVUMmt3TUdwdFNDSXNJbTFoWXlJNklqRmlZbU13TXprelpXUXdaVFptTm1RM01UZzFNRGsxTkdGaE1USXlNVEUzTTJFellqSTFZVE5rT0dJMU1EQmtZelF4WTJSaE5ERTFZVFk1WldNNVlqZ2lmUT09In0.Pf9DRaei-OYgLWgJCh0sPj9IxxsPzlZ_pd2NLuwO-DM
@@ -50,8 +45,7 @@ chrome.pageAction.onClicked.addListener(() => {
 let activeReq;
 
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-    console.log('req', req);
+    // console.log('req', req);
     activeReq = req;
-    // initActions(req, sendResponse);
 });
 
--- content.js
+++ content.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("document-title", document.title);
     setTimeout(() => {
         const iframes = document.getElementsByTagName('iframe');
-        console.log('iframes', iframes);
+        // console.log('iframes', iframes);
         const iframe = iframes[0];
         console.log('iframe', iframe);
         if (iframe) {
-            console.log(iframe.src);
+            // console.log(iframe.src);
             chrome.runtime.sendMessage({
                     request: 'init',
                     title: document.title,
